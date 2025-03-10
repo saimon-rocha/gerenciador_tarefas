ccf8e50ee06d1d8f096215af44622dbf1af38347
Saimon Rocha <saimon.rocha07@gmail.com>
2025-03-10 17:24:08 -0300

    ajustado nome do import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import TaskPage from './pages/taskpage.jsx'
+import TaskPage from './pages/TaskPage.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
